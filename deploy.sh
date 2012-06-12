ecae920c6e27547544cdea8644454b8b4cb173b2
Jordan Eldredge <jordaneldredge@gmail.com>
2012-06-12 13:37:22 -0700

    Simplfied deploy.sh to just link the vim files

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,9 @@
 # location via sys links
 
 # Perhaps this should move the original files out of the way first
-# Found some good info here: http://stackoverflow.com/questions/5238019/using-bash-to-automate-dotfiles
 
-# Another option would be Homsick, but wouldn't that require having it installed on the forign machine?
-# https://github.com/technicalpickles/homesick
+# Get the parent dir of deploy.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for f in ~/dotfiles/*
-do
-    if [[ $f = 'deploy.sh' ]]; then continue; fi
-    ln -s "$f" "$HOME/.${f##*/}"
-done
-
+ln -s $DIR/vim $HOME/.vim
+ln -s $DIR/vimrc $HOM/.vimrc

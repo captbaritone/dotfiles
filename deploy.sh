cdaa395105fc08ba0526bd11da1ab1073f7f7cfd
Jordan Eldredge <jordaneldredge@gmail.com>
2012-06-11 21:51:25 -0700

    Some notes in my deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@
 # Perhaps this should move the original files out of the way first
 # Found some good info here: http://stackoverflow.com/questions/5238019/using-bash-to-automate-dotfiles
 
+# Another option would be Homsick, but wouldn't that require having it installed on the forign machine?
+# https://github.com/technicalpickles/homesick
+
 for f in ~/dotfiles/*
 do
     if [[ $f = 'deploy.sh' ]]; then continue; fi

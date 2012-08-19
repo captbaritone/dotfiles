ca322ae01a04e7f9b574b52401a54f1659a88bb5
Jordan Eldredge <jordaneldredge@gmail.com>
2012-08-18 23:02:58 -0700

    Updated install process to include updating Vundle bundles. Also install no-longer installs .ssh

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-cd "~/dotfiles"
+cd ~/dotfiles
+
+echo "Updating submodules"
+git submodule update -q --init
+
+echo "Updating Vundle bundles"
+vim -u ~/.vim/bundles.vim +BundleInstall +qall
 
 echo "\nLinking dotfiles into place"
 for name in *; do
-    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ] && [ "$name" != 'tools' ]; then
+    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ] && [ "$name" != 'tools' ] && [ "$name" != 'ssh' ]; then
         target="$HOME/.$name"
         if [ "$target" -ef "$name" ]; then
             echo "OK - .$name"
@@ -21,5 +27,3 @@ done
 
 echo "\nInstallation complete. You may wish to issue the following:"
 echo "        source ~/.bashrc"
-echo "        git submodule update -q --init"
-echo "        vim +BundleInstall +qall"

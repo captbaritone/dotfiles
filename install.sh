0db94fe5e37fd1509501ea26a6e6f24ba248f959
Jordan Eldredge <jordaneldredge@gmail.com>
2012-08-23 12:19:53 -0700

    Formatting of install.sh and laying the groundwork for checking if my favorite tools are installed

--- install.sh
+++ install.sh
@@ -2,28 +2,51 @@
 
 cd ~/dotfiles
 
+# Check for some required tools
+echo "Checking for commonly used tools"
+# git
+GIT_IS_AVALIABLE=$?
+if [ $GIT_IS_AVALIABLE -eq 0 ]; then
+    echo "    OK - Git"
+else
+    echo "    MISSING - Git"
+fi
+#
+# vim
+#   +python
+#
+# curl
+#
+# Pull ~/dotfiles?
+
 echo "Updating submodules"
-git submodule update -q --init
+echo "    Vundle..."
+git submodule update -q --init ~/dotfiles/vim/bundle/vundle
+echo "    z..."
+git submodule update -q --init ~/dotfiles/tools/z
+#echo "    .ssh..."
+#git submodule update -q --init ~/dotfiles/ssh
 
 echo "Updating Vundle bundles"
 vim -u ~/.vim/bundles.vim +BundleInstall +qall
+echo "    Done"
 
 echo "\nLinking dotfiles into place"
 for name in *; do
     if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ] && [ "$name" != 'tools' ] && [ "$name" != 'ssh' ]; then
         target="$HOME/.$name"
         if [ "$target" -ef "$name" ]; then
-            echo "OK - .$name"
+            echo "    OK - .$name"
         else
             if [ -e "$target" ]; then
                 mv $target "$target.local"
-                echo "MOVED - $target to $target.local"
+                echo "    MOVED - $target to $target.local"
             fi
             ln -s "$PWD/$name" "$target"
-            echo "INSTALLED .$name"
+            echo "    INSTALLED .$name"
         fi
     fi
 done
 
 echo "\nInstallation complete. You may wish to issue the following:"
-echo "        source ~/.bashrc"
+echo "    source ~/.bashrc"

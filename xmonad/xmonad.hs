487112ebe170cccd9c9951ceae245decdcbd71bb
Jordan Eldredge <captbaritone@gmail.com>
2013-08-09 00:20:11 -0700

    Xmonad startup scripts

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,7 +6,7 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 myManageHook = composeAll
-    [ className =? "Google-chrome"       --> doShift "3"
+    [ className =? "Google-chrome" --> doShift "3:web"
     , className =? "Vncviewer"  --> doFloat
     ]
 
@@ -19,9 +19,22 @@ xmonad $ defaultConfig
      , workspaces = ["term","vim","web"]
      , focusedBorderColor = "#ffffff"
      , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
+     , startupHook = do
+        spawn "~/.xmonad/startup-hook"
+        -- Map capslock to esc
+        spawn "xmodmap -e \"remove Lock = Caps_Lock\""
+        spawn "xmodmap -e \"keysym Caps_Lock = Escape\""
+        --spawnOn "2:vim" "gvim"
+        spawn "gvim"
+        spawn "google-chrome"
+        spawn "gnome-terminal"
+        spawn "trayer --edge top --align right --SetDockType true --SetPartialStrut false --expand true --width 5 --transparent true --tint 0x000000 --height 17"
+        spawn "dropbox start"
+        spawn "gnome-settings-daemon"
+        spawn "nm-applet"
      , layoutHook = avoidStruts  $  layoutHook defaultConfig
      , logHook = dynamicLogWithPP $ xmobarPP
         { ppOutput = hPutStrLn xmproc
-        , ppTitle = xmobarColor "green" "" . shorten 50
+        , ppTitle = shorten 50
         }
      }

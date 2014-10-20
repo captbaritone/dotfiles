7d60da86c48eb77a30773d06261962adae3b856e
Jordan Eldredge <jeldredge@hearsaycorp.com>
2014-10-20 02:26:02 +0000

    Update ubuntu packages

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -3,11 +3,24 @@
 command -v apt-get || exit
 
 echo "Installing Ubuntu apt-get packages"
+
+# For mosh
+sudo apt-get install python-software-properties
+sudo add-apt-repository -y ppa:keithw/mosh
+
+# For neovim
+sudo add-apt-repository -y ppa:rudenko/neovim 
+
 sudo apt-get update
 sudo apt-get install --assume-yes \
     curl \
     git \
+    locate \
     lynx \
+    mosh \
+    neovim \
+    php5 php5-cli \
+    python-software-properties \
     silversearcher-ag \
     tree \
     vim \

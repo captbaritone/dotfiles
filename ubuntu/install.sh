401e8e41115500d5314482ff219b9959a3508185
Jordan Eldredge <jeldredge@hearsaycorp.com>
2015-11-12 01:05:41 +0000

    Give up on Mosh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -4,10 +4,6 @@ command -v apt-get || exit
 
 echo "Installing Ubuntu apt-get packages"
 
-# For mosh
-sudo apt-get install python-software-properties
-sudo add-apt-repository -y ppa:keithw/mosh
-
 # For neovim
 sudo add-apt-repository ppa:neovim-ppa/unstable
 
@@ -18,7 +14,6 @@ sudo apt-get install --assume-yes \
     git \
     locate \
     lynx \
-    mosh \
     neovim \
     php5 php5-cli \
     python-software-properties \

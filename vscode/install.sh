c7f6a1a3574f95a2a5b229b889158142c26dcab8
Jordan Eldredge <jordan@jordaneldredge.com>
2017-08-29 09:20:51 -0700

    Move VSCode config to dotfiles

---
+++ vscode/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/dotfiles/vscode/snippets/ ~/Library/Application\ Support/Code/User/snippets
